4a4573ba89488e2c2e2542ae80b666286cb1c746
LopCuns <jorgelopezcuns@gmail.com>
2023-06-16 17:49:54 +0200

    cors added

--- src/config/express.js
+++ src/config/express.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import userRouter from '#Routes/user.routes.js'
 import postsRouter from '#Routes/post.routes.js'
+import cors from 'cors'
 // Crear servidor de express
 const expressApp = express()
 
 // MIDDLEWARES
 expressApp.use(express.json())
+expressApp.use(cors())
 // ROUTERS
 expressApp.use('/user', userRouter)
 expressApp.use('/posts', postsRouter)
